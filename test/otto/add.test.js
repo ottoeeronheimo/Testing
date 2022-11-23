c23adcc204cd990c46339b09e22ca39d8468b25c
ottoeeronheimo <oeeronheimo@gmail.com>
2022-11-23 12:22:24 +0000

    Added failing tests into CI pipeline

--- test/otto/add.test.js
+++ test/otto/add.test.js
@@ -1,17 +1,37 @@
 import add from '../../src/add.js';
 
-test('to pass: add 2 + 2', () => {
+test('to pass: 2 + 2 = 4', () => {
     expect(add(2, 2)).toBe(4);
 });
 
-test('to pass: add -2 + 2', () => {
+test('to pass: -2 +  = 0', () => {
     expect(add(-2, 2)).toBe(0);
 });
 
-test('to pass: add 0 + 0', () => {
+test('to pass: 0 + 0 = 0', () => {
     expect(add(0, 0)).toBe(0);
 });
 
-test('to pass: add -2 + -2', () => {
+test('to pass: -2 + -2 = -4', () => {
     expect(add(-2, -2)).toBe(-4);
+});
+
+test('to pass: Infinity + -Infinity = NaN', () => {
+    expect(add(Infinity, -Infinity)).toBe(NaN);
+});
+
+test('to pass: text + 1 = text1', () => {
+    expect(add("text", 1)).toEqual("text1");
+});
+
+test('to pass: null value is handled', () => {
+    expect(add(null, 1)).toEqual(1);
+});
+
+test('to pass: undefined values are handled', () => {
+    expect(add(undefined, undefined)).toEqual(0);
+});
+
+test('to pass: Error value is handled', () => {
+    expect(add(Error, 1)).toEqual(NaN);
 });
--- test/otto/clamp.test.js
+++ test/otto/clamp.test.js
@@ -1,9 +1,82 @@
 /**
  * Package contains unit tests for clamp.js function
- * Currently dummy test case for preventing error (Your test suite must contain at least one test.)
+ * Example of desired behavior from https://css-tricks.com/snippets/sass/clamping-number/
+    $clamp: clamp(42, $min: 0, $max: 1337);    // 42
+    $clamp: clamp(42, $min: 1337, $max: 9000); // 1337
+    $clamp: clamp(42, $min: 0, $max: 1);       // 1
 */
-import add from '../../src/add.js';
+import clamp from '../../src/clamp.js';
 
-test('to pass: add 2 + 2', () => {
-    expect(add(2, 2)).toBe(4);
+// Test case from clamp.js file
+test('to pass: clamp(-10, -5, 5)= -5', () => {
+    expect(clamp(-10, -5, 5)).toBe(-5);
+});
+
+// test zero within zero boundaries
+test('to pass: clamp(0, 0, 0) = 0', () => {
+    expect(clamp(0, 0, 0)).toBe(0);
+});
+
+// Test negative number in positive boundaries
+test('to pass: clamp(100, 5, 10) = 10', () => {
+    expect(clamp(-100, 5, 10)).toBe(5);
+});
+
+// Test positive number in same but bigger boundaries
+test('to pass: clamp(5, 10, 10) = 10', () => {
+    expect(clamp(5, 10, 10)).toBe(10);
+});
+
+// Test positive number in same but smaller boundaries
+test('to pass: clamp(20, 10, 10) = 10', () => {
+    expect(clamp(20, 10, 10)).toBe(10);
+});
+
+// Test handling error
+test('to pass: clamp(Error, 10, 10) = 10', () => {
+    expect(clamp(Error, 10, 10)).toBe(NaN);
+});
+
+// Test clamp inside negative boundaries
+test('to pass: clamp(-7,-5, -10) = -7', () => {
+    expect(clamp(-7,-5, -10)).toBe(-7);
+});
+
+/**
+ * FAILING TEST CASES
+*/
+
+// Test case from clamp.js file
+test('to pass: clamp(10, -5, 5)= 5', () => {
+    expect(clamp(10, -5, 5)).toBe(5);
+});
+
+// Test positive number in positive boundaries
+test('to pass: clamp(100, 5, 10) = 10', () => {
+    expect(clamp(100, 5, 10)).toBe(10);
+});
+
+// Test handling NaN type
+test('to pass: clamp(20, NaN, 10) = 10', () => {
+    expect(clamp(20, NaN, 10)).toBe(10);
+});
+
+// Test clamp inside positive boundaries
+test('to pass: clamp(7,5, 10) = 7', () => {
+    expect(clamp(7,5, 10)).toBe(7);
+});
+
+// Test clamp inside negative boundaries with bigger numbers
+test('to pass: clamp(-50,-100, -10) = -50', () => {
+    expect(clamp(-50,-100, -10)).toBe(-50);
+});
+
+// Test clamp inside boundary from 0 to 100
+test('to pass: clamp(40,0, 100) = 40', () => {
+    expect(clamp(40,0, 100)).toBe(40);
+});
+
+// Test clamp inside negative and positive boundaries
+test('to pass: clamp(0,-5, 5) = 0', () => {
+    expect(clamp(0,-5, 5)).toBe(0);
 });
