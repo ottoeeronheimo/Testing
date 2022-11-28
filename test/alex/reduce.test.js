73f573e0896d38d86af57f7ed65d20dfc92aa2bc
Alex Malkki <alex.malkki@tuni.fi>
2022-11-28 20:03:18 +0200

    reduce.test.js (failing)

--- test/alex/reduce.test.js
+++ test/alex/reduce.test.js
@@ -1,5 +1,89 @@
 import reduce from '../../src/reduce';
 
-test('dummy test', () => {
-    expect(1).toBe(1);
+test('sum array values', () => {
+
+    const a = [1,2,3,4,5];
+
+    const func = (acc, val) => acc + val;
+
+    expect(reduce(a, func)).toEqual(15);
+    expect(reduce(a, func, 5)).toEqual(20);
+});
+
+test('sum array indexes', () => {
+
+    const a = [1,2,3,4,5];
+
+    const func = (acc, val, i) => acc + i;
+
+    expect(reduce(a, func)).toEqual(11);
+    expect(reduce(a, func, 5)).toEqual(15);
+});
+
+test('sum obj values', () => {
+    
+    const o = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 5
+    };
+
+    const func = (acc, val) => acc + val;
+
+    expect(reduce(o, func)).toEqual(15);
+    expect(reduce(o, func, 5)).toEqual(20);
+});
+
+// Failing test!
+// reduce() skips running the iteratee for the first time
+// when initial accumulator value is not supplied to reduce().
+// This is appearent if lines 58 and 62 are uncommented and 
+// lines 62 and 63 switched between test runs.
+/*
+test('sum obj keys', () => {
+    
+    const o = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 5
+    };
+
+    const func = (acc, val, key) => {
+        //console.log(acc, key);
+        return acc + key;
+    };
+
+    //expect(reduce(o, func)).toEqual('1abcde');
+    expect(reduce(o, func, 22)).toEqual('22abcde');
+});
+*/
+
+test('reduce obj values', () => {
+    
+    const o = {
+        a: 1,
+        b: 1,
+        c: 3,
+        d: 2,
+        e: 2
+    };
+
+    const func = (res, val, key) => {
+        if (!res[val]) res[val] = [];
+        res[val].push(key);
+        return res;
+    };
+
+    const res_ = {
+        1: ['a', 'b'],
+        2: ['d', 'e'],
+        3: ['c']
+    }
+
+    expect(reduce(o, func, {})).toEqual(res_);
+    expect(() => reduce(o, func)).toThrow(TypeError);
 });
